961bb4a8d13c78f78d87f4616b8f6a590a72dade
Don Cross <cosinekitty@gmail.com>
2025-08-18 10:26:38 -0400

    Added unit test stub.

--- src/zoosearch.cpp
+++ src/zoosearch.cpp
@@ -1,6 +1,25 @@
 #include <cstdio>
+#include <cstring>
+
+
+static int UnitTests();
+
 
 int main(int argc, const char *argv[])
 {
+    if (argc >= 2)
+    {
+        const char *verb = argv[1];
+        if (0 == strcmp(verb, "test"))
+            return UnitTests();
+    }
+    printf("zoosearch: Invalid command line arguments.\n");
+    return 1;
+}
+
+
+static int UnitTests()
+{
+    printf("UnitTests: PASS\n");
     return 0;
 }
