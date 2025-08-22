68be42ab5837ef16add3516f901346224bce4e6f
Don Cross <cosinekitty@gmail.com>
2025-08-21 21:30:37 -0400

    expressions.py: all possible 1-op expressions.

--- prototype/expressions.py
+++ prototype/expressions.py
@@ -3,11 +3,26 @@
 import sys
 from typing import Generator
 
+Vars = 'abcdxyz'
+NumVars = len(Vars)
+
 def PostfixExpressions(exactOperatorCount:int) -> Generator[str, None, None]:
     if exactOperatorCount == 0:
-        for c in 'abcdxyz':
-            yield c
+        for u in 'abcdxyz':
+            yield u
+        return
+
+    if exactOperatorCount == 1:
+        for u in 'abcdxyz':
+            for v in 'abcdxyz':
+                if u < v:
+                    yield u + v + '-'
+                    yield v + u + '-'
+                    yield u + v + '+'
+                    yield u + v + '*'
+        return
 
 if __name__ == '__main__':
     print(', '.join(PostfixExpressions(0)))
+    print(', '.join(PostfixExpressions(1)))
     sys.exit(0)
